02fd58d725e40199772dde1ff7f4ab23ffeed328
kajeagentspi <jptagnepis@up.edu.ph>
2017-07-12 23:30:42 +0800

    Fix some errors
    Implemented multi download

--- theripper.sh
+++ theripper.sh
@@ -3,59 +3,66 @@
 ##################################################################
 # Description: Uses wget's spider with aria2c's parallel downloading
 # Usage: ./theripper.sh "opendirlink" "opendirsubstring"
-# Example ./theripper.sh "libgen.io/comics0/_DC/100%20Bullets%20-%20Deluxe%20Edition%20%282013%29%20%5b1-5%5d/" "libgen.io/comics0/_DC"
-#
-# TODO: Implement GNU Parallel so two or more files can be downloaded at the same time.
+# Example: ./theripper.sh "link.com/blabla/doraemon/" "link.com/blabla"
+# Note: Make sure that the path doesn't contain %20's or others. You can use this site http://www.asiteaboutnothing.net/c_decode-url.html
 ####################################################################
 
 set -e
 
 URL=$1
 ROOT_PATH=$2
-LIST=$TMP/list-$$.txt
+LIST=./list-$$.txt
 MAX_CONNECTIONS_PER_SERVER=16
 
 usage() {
-  cat <<EOF
+	cat <<EOF
 Uses wget's spider with aria2c's parallel for downloading open
 directories.
-
 Usage: $SCRIPT_NAME [options] URL PATH
 EOF
 }
 
 spider() {
-  local logfile=$TMP/opendir-$$.log
-  wget -o $logfile -e robots=off -r --no-parent --spider "$URL"
-  cat $logfile | grep -i Removing | sed -e "s/Removing //g" | \
-    sed 's/.$//' | sed '/index.html/d' > $LIST
-  echo $URL | sed 's/[/].*$//' | xargs rm -rf
+	local logfile=./opendir-$$.log
+	wget -o $logfile -e robots=off -r --no-parent --spider "$URL"
+	cat $logfile | grep -i Removing | sed -e "s/Removing //g" | \
+	sed 's/.$//' | sed '/index.html/d' > $LIST
+
+	#Delete the folder made by wget
+	echo $ROOT_PATH | sed 's/[/].*$//' | xargs rm -rf
 }
 
 download() {
-  while read link; do
-      echo "Downloading $link"
+	while read link; do
 
-      # Remove text after last /
-      FULL_PATH=$(echo $link | sed 's%/[^/]*$%/%')
-      FILE_PATH=${FULL_PATH#${ROOT_PATH}/}
+		# Remove text after last /
+		FULL_PATH=$(echo $link | sed 's%/[^/]*$%/%')
+		FILE_PATH=${FULL_PATH#${ROOT_PATH}/}
 
-      echo "Saving to $FILE_PATH"
-      # Since the links in the file doesn't have an identifier aria2c will error
-      IDN="http"
-      if [[ ${URL:0:5} == "https" ]]; then
-        IDN="https"
-      fi
-      DOWNLOAD_LINK=$(echo $link | sed -e "s/$URL/$IDN:\/\/$URL/g")
-      aria2c --continue=true \
-        --max-connection-per-server=$MAX_CONNECTIONS_PER_SERVER \
-        --split=16 --min-split-size=1M --dir="$FILE_PATH" "$DOWNLOAD_LINK"
-  done  < $LIST
+		# Since the links in the file doesn't have an identifier aria2c will error
+		IDN="http://"
+		if [[ ${URL:0:5} == "https" ]]; then
+		IDN="https://"
+		elif [[ ${URL:0:3} == "ftp" ]]; then
+		IDN="ftp://"
+		fi
+		DOWNLOAD_LINK=$(echo "$IDN$link")
+		#Enclosed the Downlaod link in quotes since echo replaces multiple spaces with a single one
+		echo "${DOWNLOAD_LINK}" >> link-$$.down
+		echo " dir=$FILE_PATH" >> link-$$.down
+		echo " continue=true" >> link-$$.down
+		echo " max-connection-per-server=$MAX_CONNECTIONS_PER_SERVER" >> link-$$.down
+		echo " split=16" >> link-$$.down
+		echo -e " min-split-size=1M\n" >> link-$$.down
+	done  < $LIST
+	#Download links
+	aria2c -i link-$$.down -j 10
+
 }
 
-if [[ -z $1 || -z $2 ]]; then
-  usage
-  exit 1
+if [[ -z $1 || -z $2 || $# -ge 3 ]]; then
+	usage
+	exit 1
 fi
 
 echo "Creating list of urls..."
@@ -64,4 +71,7 @@ echo "Index created!"
 download
 
 # Cleanup
-trap "rm -f '$LIST'" EXIT
+rm opendir-$$.log
+rm list-$$.txt
+rm link-$$.down
+
