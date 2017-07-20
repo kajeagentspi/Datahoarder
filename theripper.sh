94734da7e654816b044a28f28cec81f4c28eecc7
kajeagentspi <jptagnepis@up.edu.ph>
2017-07-20 12:29:10 +0800

    Update theripper.sh

--- theripper.sh
+++ theripper.sh
@@ -3,8 +3,7 @@
 ##################################################################
 # Description: Uses wget's spider with aria2c's parallel downloading
 # Usage: ./theripper.sh "opendirlink" "opendirsubstring"
-# Example: ./theripper.sh "link.com/blabla/doraemon/" "link.com/blabla"
-# Note: Make sure that the path doesn't contain %20's or others. You can use this site http://www.asiteaboutnothing.net/c_decode-url.html
+# Example: ./theripper.sh "http://link.com/blabla/doraemon/" "http://link.com/blabla"
 ####################################################################
 
 set -e
