5325ee388df65ee9059776cb646a8eebdea519e3
j0hnsen <j0hnsen@users.noreply.github.com>
2017-07-25 21:14:05 +0200

    Some improvements and fixes for broken downloads

--- theripper.sh
+++ theripper.sh
@@ -24,9 +24,15 @@ EOF
 
 spider() {
 	local logfile=./opendir-$$.log
-	wget -o $logfile -e robots=off -r --no-parent --spider "$URL" 
+	wget -o $logfile -e robots=off -r --no-parent --spider "$URL" || true
 	#Grabs all lines with the pattern --2017-07-12 15:40:31-- then from the results removes everthing that ends in / (meaning it's a directory
 	#then removes pattern from every line
+	grep -B 2 -E '... 404 Not Found|... 403 Forbidden|... 301 Moved Permanently' $logfile | \
+	grep -i '^--[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]--' | \
+	grep '[^'/']$'  | sed -e 's/^--[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]--  //g' > $logfile.tmp
+	while read line; do
+		sed -i "\|$line|d" $logfile
+	done < $logfile.tmp
 	cat $logfile | grep -i '^--[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]--' | \
 	grep '[^'/']$'  | sed -e 's/^--[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]--  //g' > $LIST
 	#Delete the folder made by wget (deletes all empty directories in the directory this script is run
@@ -46,6 +52,8 @@ download() {
 		echo " continue=true" >> link-$$.down
 		echo " max-connection-per-server=$MAX_CONNECTIONS_PER_SERVER" >> link-$$.down
 		echo " split=16" >> link-$$.down
+		echo " user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:52.0) Gecko/20100101 Firefox/52.0" >> link-$$.down
+		echo " header=Accept: text/html" >> link-$$.down
 		echo -e " min-split-size=1M\n" >> link-$$.down
 	done  < $LIST
 	#Download links
@@ -65,6 +73,6 @@ download
 
 # Cleanup
 rm opendir-$$.log
+rm opendir-$$.log.tmp
 rm list-$$.txt
 rm link-$$.down
-
